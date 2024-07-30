7f57528263aa59fd0f1bae413927534162ccaa1e
Sai kumar Silaparasetti <76764544+saikumarsilaparasetti@users.noreply.github.com>
2024-07-31 00:52:08 +0530

    deleted firebase token

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAcvVQ-u1cHZNBinMaI2aZriRYCpB0ozf8",
-    authDomain: "whatsapp-clone-797cb.firebaseapp.com",
-    projectId: "whatsapp-clone-797cb",
-    storageBucket: "whatsapp-clone-797cb.appspot.com",
-    messagingSenderId: "296839087010",
-    appId: "1:296839087010:web:934013fe6ce3eaa66968fb"
-  };
